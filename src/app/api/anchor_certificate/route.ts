7e4106efc36ab2b801fd88ca110df38700a3d14d
yuyuabdinoor <yuyuabdinoor@gmail.com>
2025-06-07 14:10:00 +0300

    Connected the anchor certificate API to the database

--- src/app/api/anchor_certificate/route.ts
+++ src/app/api/anchor_certificate/route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+ 
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextResponse } from "next/server";
